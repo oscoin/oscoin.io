bc20264bbd1cfd9b7618347b462d87ffd2b116fa
Alexander Simmerl <a.simmerl@gmail.com>
2019-09-20 19:47:41 +0200

    Implement update layout and add first entry

--- pages/updates.js
+++ pages/updates.js
@@ -3,30 +3,65 @@ import PropTypes from 'prop-types'
 
 import Page from 'layouts/page'
 
-const Updates = ({ updates }) => (
+const ReleaseDate = ({date}) => (
+  <>
+    {date.getDate()} {date.toLocaleDateString('default', {month: 'short'})} {date.getFullYear()}
+  </>
+)
+
+ReleaseDate.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired
+}
+
+const Updates = ({updates}) => (
   <Page>
-    <h2>Updates</h2>
-    {updates.map(update => (
-      <Link href={`/updates/${update.id}`} key={`update-${update.id}`}>
-        <a>{update.meta.title}</a>
-      </Link>
-    ))}
+    <h1>Updates</h1>
+    <div className="updates">
+      {updates.map(update => (
+        <div key={`update-${update.id}`}>
+          <ReleaseDate date={new Date(update.meta.releaseDate)} />
+          <span> — </span>
+          <Link href={`/updates/${update.id}`}>
+            <a>{update.meta.title}</a>
+          </Link>
+        </div>
+      ))}
+    </div>
   </Page>
 )
 
-Updates.protoTypes = {
-  updates: PropTypes.array
+Updates.propTypes = {
+  updates: PropTypes.array.isRequired
 }
 
 Updates.defaultProps = {
   updates: []
 }
 
+const getFiles = () => {
+  const files =
+    preval`module.exports = require('fs').readdirSync("./pages/updates")` || []
+
+  return Promise.resolve(files)
+}
+
+const getUpdates = async () => {
+  const files = await getFiles()
+  const updates = files.reduce((collection, file) => {
+    collection.push({
+      meta: require(`../pages/updates/${file}`).meta,
+      id: file.replace(/\.[^/.]+$/, '')
+    })
+
+    return collection
+  }, [])
+
+  return updates
+}
+
 Updates.getInitialProps = async () => {
   return {
-    updates: [
-      {
-    ]
+    updates: await getUpdates()
   }
 }
 
